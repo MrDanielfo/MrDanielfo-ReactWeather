4039b2b31c1687f88af4596c485b73bb3f930cf7
Josué Daniel Flores <mr.danielfo@gmail.com>
2019-02-26 15:51:09 -0600

    Primer Commit

---
+++ precache-manifest.c3aa18e61d6ecf2d017691b159af6481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eeab715935e8a8038c89",
+    "url": "/static/js/main.eeab7159.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "eeab715935e8a8038c89",
+    "url": "/static/css/main.a037a71e.chunk.css"
+  },
+  {
+    "revision": "ecf6fafbc095ee54e8d0431488c535e8",
+    "url": "/index.html"
+  }
+];
